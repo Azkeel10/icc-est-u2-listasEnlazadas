30f67fcd612ebfe089dca39ce606143d1fd1af78
Azkeel10 <jismalt26@gmail.com>
2025-06-11 17:18:15 -0500

    detalles

--- src/Controllers/ContactManager.java
+++ src/Controllers/ContactManager.java
@@ -8,33 +8,21 @@ public class ContactManager {
     private LinkedList<Contact<String, String>> contacts = new LinkedList<>();
 //-------------------------------------------------------------------------------
     public void addContact(Contact<String, String> contact) {
-        contacts.appendToTail(contact);
+        
     }
 //-------------------------------------------------------------------------------
     public Contact<String, String> findContactByName(String name) {
-        Node<Contact<String, String>> current = contacts.getHead();
-        while (current != null) {
-            if (current.getValue().getName().equals(name)) {
-                return current.getValue();
-            }
-            current = current.getNode();
-        }
+        
         return null;
     }
 //-------------------------------------------------------------------------------
     public void deleteContactByName(String name) {
-        Node<Contact<String, String>> current = contacts.getHead();
-        while (current != null) {
-            if (current.getValue().getName().equals(name)) {
-                contacts.deleteByValue(current.getValue());
-                return;
-            }
-            current = current.getNext();
-        }
+        
+        
     }
 //-------------------------------------------------------------------------------
     public void printList() {
-        contacts.print();
+        
     }
 //-------------------------------------------------------------------------------
 }
